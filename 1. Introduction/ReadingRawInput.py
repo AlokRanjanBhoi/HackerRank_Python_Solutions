50958125c7155c74533833f55cb42269b7e07751
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-01-14 23:32:08 +0530

    Add files via upload

---
+++ 1. Introduction/ReadingRawInput.py
@@ -0,0 +1,10 @@
+'''
+Title     : Reading Raw Input
+Subdomain : Introduction
+Domain    : Python3
+Author    : Alok Ranjan Bhoi
+Created   : 14 January 2019
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+name=input()
+print(name)
