96965108c77c3dd4f27ac10101dfc98578340845
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-01-14 23:33:37 +0530

    Add files via upload

---
+++ 1. Introduction/PrintFunction.py
@@ -0,0 +1,11 @@
+'''
+Title     : Print Function
+Subdomain : Introduction
+Domain    : Python3
+Author    : Alok Ranjan Bhoi
+Created   : 14 January 2019
+'''
+n=int(input())
+ar=range(1,n+1)
+for i in ar:
+    print(i,end="")
