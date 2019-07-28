933fe0c7f159e7b51e398e509eabb9c5085341d2
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-07-28 23:49:40 +0530

    Add files via upload

---
+++ 5. Math/IntegersComeInAllSizes.py
@@ -0,0 +1,14 @@
+'''
+Title     : Integers Come In All Sizes
+Subdomain : Math
+Domain    : Python3
+Author    : Alok Ranjan Bhoi
+Created   : 28 July 2019
+Problem   : https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(pow(a,b)+pow(c,d))
