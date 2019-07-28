b482426c0d28b079668e2667f09ff24a7dce923e
Alok Ranjan Bhoi <alok.ranjan.bhoi@suiit.ac.in>
2019-07-29 00:24:09 +0530

    Add files via upload

---
+++ 5. Math/ModDivmod.py
@@ -0,0 +1,15 @@
+'''
+Title     : Mod Divmod
+Subdomain : Math
+Domain    : Python3
+Author    : Alok Ranjan Bhoi
+Created   : 28 July 2019
+Problem   : https://www.hackerrank.com/challenges/python-mod-divmod/problem
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+d=divmod(a,b)
+print(d[0])
+print(d[1])
+print(d)
